a0b5c590285ce990fefc08112bd816a3d97e759f
Sumant-Dusane <sumant.dusane1966@gmail.com>
2024-04-08 02:44:54 +0530

    added chaining

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 var loaders = [];
+var isFirst = true;
 const main = document.getElementById('main123');
 
 btn.addEventListener('click', () => {
     addLoaderDOM();
-    addLoader();
+    if(isFirst) {
+        isFirst = false;
+        addLoader();
+    }
 });
 
 const addLoaderDOM = () => {
@@ -12,13 +16,15 @@ const addLoaderDOM = () => {
     const childLoader = document.createElement('span');
     childLoader.classList.add('child');
     parentLoader.appendChild(childLoader);
-    main.appendChild(parentLoader);
     loaders.push(parentLoader);
 }
 
 const addLoader = () => {
     const currentLoader = loaders.shift();
+    if (!currentLoader) return
     const currentChildLoader = currentLoader.querySelector('.child');
+
+    main.append(currentLoader);
     var count = 0;
     var loaderIntervalref = setInterval(() => {
         console.log(count, loaderIntervalref);
@@ -27,7 +33,15 @@ const addLoader = () => {
             currentChildLoader.style.width = `${count}%`;
         } else {
             clearInterval(loaderIntervalref);
+            addLoader();
         }
     }, 30);
 }
 
+/*
+1. onclick -> creating htmk and pushing in array 
+2. if clickfirst flag is true then start the chainng function
+3. chaining function is addLoader  
+    3.1 which will one by one get array's 1st element and append to DOM
+    3.2 execute our loading functionality and apply recursion to apply chaning rule until undefined or invalid element is passed which wil be the base case
+*/
